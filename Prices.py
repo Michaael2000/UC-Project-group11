8f3eb173ea70ab08e6e76bae1cecc2e86f934183
M. de Rooij <m.de.rooij.8@umail.leidenuniv.nl>
2022-11-17 17:55:41 +0100

    preprocessed the data

--- Prices.py
+++ Prices.py
@@ -2,22 +2,16 @@ from datetime import datetime as dt
 import pandas as pd
 
 df = pd.read_csv("df_july.csv")
-df_2 = pd.DataFrame()
-print(len(df_2))
 # df_july = df[df['TIMESTAMP'] <= 1375307999]
-# df_july.to_csv("df_july.csv")
+# df_july.to_csv("data/df_july.csv")
 
 # timestamps_july = df_july['TIMESTAMP']
-
-
 # dates_july = timestamps_july.apply(dt.fromtimestamp)
-# print(dates_july.head())
-outliers = []
-for i in range(len(df['POLYLINE'])):
-    if len(df['POLYLINE'][i]) < 55:
-        outliers.append(i)
-        df_2 = pd.concat([df_2, ])
-print(len(outliers))
-df.drop(outliers, axis=0, inplace=True)
 
-# print(df[0])
+print(df)
+temp_df = pd.DataFrame()
+temp_df = pd.concat([temp_df, df.loc[df['POLYLINE'].str.len() < 55]], axis=0)
+df = pd.concat([df, temp_df], axis=0).drop_duplicates(keep=False)
+df.to_csv("data/df_july_clean.csv")
+print(df)
+
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # UC-Project
 
 ADAPT-Pricing
+
+Foursquare API key: fsq3rtPPvVjO0HDU3euCF6Kk95D2uz5Z2feE9/XR7fL++R8=
